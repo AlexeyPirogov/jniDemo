7354d51725c26f3be637b97563a50537e4a94a2e
Pirogov Alexey <pirogov.alexey@gmail.com>
2018-11-28 21:11:57 +0800

    Update JNIDemo.c
    format sources;

--- src/cpp/JNIDemo.c
+++ src/cpp/JNIDemo.c
@@ -4,30 +4,24 @@
 #include <unistd.h>
 #include "jnidemo_JNIDemoJava.h"
 
-JNIEXPORT jint JNICALL Java_jnidemo_JNIDemoJava_nativeCrash
-        (JNIEnv *env, jobject obj)
-{
+JNIEXPORT jint JNICALL Java_jnidemo_JNIDemoJava_nativeCrash(JNIEnv *env, jobject obj) {
       const char *s = NULL;
       printf( "%c\n", s[0] );
       return 0;
 }
 
 
-JNIEXPORT jint JNICALL Java_jnidemo_JNIDemoJava_nativePrint
-        (JNIEnv *env, jobject obj)
-{
+JNIEXPORT jint JNICALL Java_jnidemo_JNIDemoJava_nativePrint(JNIEnv *env, jobject obj) {
     printf("\nHello World from C!\n");
     return 0;
 }
 
-JNIEXPORT jint JNICALL Java_jnidemo_JNIDemoJava_nativeSleep
-        (JNIEnv *env, jobject obj, jint ms)
-{
+JNIEXPORT jint JNICALL Java_jnidemo_JNIDemoJava_nativeSleep(JNIEnv *env, jobject obj, jint ms) {
     usleep(ms);
     return 0;
 }
 
-JNIEXPORT jobjectArray internalNativeAllocate(JNIEnv const *env, jint objNumber) {// Get a class reference for java.lang.Double
+JNIEXPORT jobjectArray internalNativeAllocate(JNIEnv const *env, jint objNumber) {
     jclass classDouble = (*env)->FindClass(env, "java/lang/Double");
     jobjectArray outJNIArray = (*env)->NewObjectArray(env, objNumber, classDouble, NULL);
 
@@ -41,9 +35,7 @@ JNIEXPORT jobjectArray internalNativeAllocate(JNIEnv const *env, jint objNumber)
     return outJNIArray;
 }
 
-jobjectArray JNICALL Java_jnidemo_JNIDemoJava_nativeAllocate
-(JNIEnv *env, jobject obj, jint objNumber) {
+jobjectArray JNICALL Java_jnidemo_JNIDemoJava_nativeAllocate(JNIEnv *env, jobject obj, jint objNumber) {
     return internalNativeAllocate(env, objNumber);
-
 }
 
